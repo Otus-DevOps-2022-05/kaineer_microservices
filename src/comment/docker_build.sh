8ce20061eb5c15793a4b14718b906c621eb81ad8
Sergey Kluchkovsky <kaineer@gmail.com>
2022-08-20 17:05:58 +0500

    Возвращает в docker_build.sh построение образов
    Если USER_NAME определено как travisuser

--- src/comment/docker_build.sh
+++ src/comment/docker_build.sh
@@ -2,5 +2,7 @@
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
-#
-# docker build -t $USER_NAME/comment .
+
+if [[ "$USER_NAME" == "travisuser" ]]; then
+  docker build -t $USER_NAME/comment .
+fi
