aaeeaa3fd8646fd82a6cfb3a1497902e66a7268e
Sergey Kluchkovsky <kaineer@gmail.com>
2022-08-20 18:41:23 +0500

    Make ui run with alpine ruby

--- src/scripts/run_env.sh
+++ src/scripts/run_env.sh
@@ -8,10 +8,12 @@ POST=post_1
 COMMENT=comment_1
 
 COMMENT_IMAGE="kaineer/comment:2.0"
-POST_IMAGE="kaineer/post"
+POST_IMAGE="kaineer/post:latest"
+UI_IMAGE="kaineer/ui:2.0"
 
 docker run -d --network=reddit \
   --rm \
+  --volume reddit_db:/data/db \
   --network-alias=${POST_DB} \
   --network-alias=${COMMENT_DB} mongo:latest
 
@@ -29,4 +31,4 @@ docker run -d --network=reddit \
   --rm \
   --env POST_SERVICE_HOST=${POST} \
   --env COMMENT_SERVICE_HOST=${COMMENT} \
-  -p 9292:9292 kaineer/ui:latest
+  -p 9292:9292 ${UI_IMAGE}
