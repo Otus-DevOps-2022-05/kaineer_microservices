a4ed3c81052df0292dae6d3a51d1aa643788330c
Sergey Kluchkovsky <kaineer@gmail.com>
2022-08-20 18:27:40 +0500

    Переносит src/comment/Dockerfile на ruby:2-alpine

--- src/scripts/run_env.sh
+++ src/scripts/run_env.sh
@@ -2,23 +2,31 @@
 
 # docker network create reddit
 
-docker run -d --network=reddit \
-  --rm \
-  --network-alias=post-db_1 \
-  --network-alias=comment-db_1 mongo:latest
+POST_DB=post-db_1
+COMMENT_DB=comment-db_1
+POST=post_1
+COMMENT=comment_1
+
+COMMENT_IMAGE="kaineer/comment:2.0"
+POST_IMAGE="kaineer/post"
 
 docker run -d --network=reddit \
   --rm \
-  --env POST_DATABASE_HOST=post-db_1 \
-  --network-alias=post_1 kaineer/post:latest
+  --network-alias=${POST_DB} \
+  --network-alias=${COMMENT_DB} mongo:latest
 
 docker run -d --network=reddit \
   --rm \
-  --env COMMENT_DATABASE_HOST=comment-db_1 \
-  --network-alias=comment_1 kaineer/comment:latest
+  --env POST_DATABASE_HOST=${POST_DB} \
+  --network-alias=${POST} kaineer/post:latest
 
 docker run -d --network=reddit \
   --rm \
-  --env POST_SERVICE_HOST=post_1 \
-  --env COMMENT_SERVICE_HOST=comment_1 \
+  --env COMMENT_DATABASE_HOST=${COMMENT_DB} \
+  --network-alias=${COMMENT} ${COMMENT_IMAGE}
+
+docker run -d --network=reddit \
+  --rm \
+  --env POST_SERVICE_HOST=${POST} \
+  --env COMMENT_SERVICE_HOST=${COMMENT} \
   -p 9292:9292 kaineer/ui:latest
