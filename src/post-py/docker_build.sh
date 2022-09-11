bde7c773d2c40676724ef08029ce39658afbd9ec
Sergey Kluchkovsky <kaineer@gmail.com>
2022-09-11 18:43:49 +0500

    Использует Docker вместо Docker.alpine

--- src/post-py/docker_build.sh
+++ src/post-py/docker_build.sh
@@ -4,7 +4,7 @@ echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
 
 if [ "$USER_NAME" = "travisuser" ]; then
-  docker build -t "$USER_NAME/post:logging" -f Dockefile.alpine .
+  docker build -t "$USER_NAME/post:logging" -f Dockefile .
 else
   echo " × Skip docker build, running from Makefile"
 fi
